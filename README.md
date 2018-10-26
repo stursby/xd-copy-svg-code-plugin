a559f34ac7b577035b584ac9e88c7c833a268f62
Charlie Hield <charlie.hield@collemcvoy.com>
2018-10-26 10:18:43 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,20 @@
+![icon](images/icon.png)
+
+# Copy SVG Code
+
+> Adobe XD Plugin to generate SVG code from selection.
+
+## Demo
+
+![icon](.github/demo.gif)
+
+## Todo
+
+Not sure if all of these are possible, but things I'd like to add.
+
+- [ ] Add right click dialog
+- [ ] Add shortcut
+- [ ] Allow for multiple selections / Auto group selections
+- [ ] Optimize with `svgo`
+- [ ] Allow user to change default options
+- [ ] Get cooler icon (any designers out there?!)
---
+++ main.js
@@ -0,0 +1,73 @@
+// Dependencies
+const fs = require('uxp').storage.localFileSystem
+const application = require('application')
+const clipboard = require('clipboard')
+const { createDialog, error } = require('./lib/dialogs.js')
+
+// Main function
+async function copySvgCode(selection) {
+  // Error if nothing selected
+  if (!selection.hasArtwork) {
+    error('No SVG selected', 'Please select an SVG before running.')
+    return
+  }
+
+  // Error if multiple selections and not grouped
+  if (selection.items.length >= 2) {
+    error(
+      'Too many selections',
+      'Please select one SVG or group multiple together.'
+    )
+    return
+  }
+
+  // Setup tmp folder and file
+  const tmpFolder = await fs.getTemporaryFolder()
+  const file = await tmpFolder.createFile('export.svg', {
+    overwrite: true
+  })
+
+  // Rendition settings
+  const renditions = [
+    {
+      node: selection.items[0],
+      outputFile: file,
+      type: application.RenditionType.SVG,
+      minify: true,
+      embedImages: false
+    }
+  ]
+
+  // Create rendition
+  await application.createRenditions(renditions)
+
+  // Read tmp file and generate SVG code
+  const markup = await file.read()
+  const svgCode = escapeHtml(markup)
+
+  // Show output dialog
+  await createDialog({
+    title: 'SVG Output',
+    template: () => `<textarea>${svgCode}</textarea>`
+  })
+
+  // Copy to clipboard too!
+  clipboard.copyText(markup)
+}
+
+// Helper(s)
+function escapeHtml(unsafe) {
+  return unsafe
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;')
+}
+
+// Exports
+module.exports = {
+  commands: {
+    copySvgCode
+  }
+}
