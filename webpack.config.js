0f388b13060cde753a9a1216d374e02056ab2b3f
Charlie Hield <charlie.hield@collemcvoy.com>
2018-10-26 11:50:33 -0500

    Use webpack

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: __dirname,
+    filename: 'main.js',
+    libraryTarget: 'commonjs2'
+  },
+  devtool: 'none',
+  externals: {
+    uxp: 'uxp',
+    application: 'application',
+    clipboard: 'clipboard'
+  }
+}
